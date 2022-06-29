562a9810ec0d17d20cb60069b81251dd5593dc32
Travis Lawrence <travis.lawrence12@gmail.com>
2022-06-28 22:18:25 -0700

    route

--- src/tables/TeamTable.js
+++ src/tables/TeamTable.js
@@ -29,7 +29,7 @@ const TeamTable = ({ teams }) => {
         ))}
       </StyledTeamList>
       <Switch>
-        <Route path={`${match.path}/:teamName`}>
+        <Route exact path={`${match.path}/:teamName`}>
           <TeamView teams={teams} />
         </Route>
       </Switch>
